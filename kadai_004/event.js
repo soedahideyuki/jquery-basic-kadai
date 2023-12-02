8afb328086283c9df5352630e59a1a5bf043b012
soedahideyuki <kiyudehi811@gmail.com>
2023-12-02 21:59:00 +0900

    課題完了

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,9 +1,9 @@
-$(function(){
 
-$(window).on('load', function(){
+$(window).on('load' , function(){
   console.log('loadイベントが発生しました。');
   });
 
+$(function(){
 $(window).on('scroll',function(){
   console.log('scrollイベントが発生しました。');
   });
